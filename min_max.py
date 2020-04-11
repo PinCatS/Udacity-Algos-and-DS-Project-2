ae7bdd4a43f2681cbabc16b44ef2e3f321b13a5b
Sergey Li <pincats@gmail.com>
2020-04-11 18:29:44 +0300

    feat: Add min max problem implementation

---
+++ min_max.py
@@ -0,0 +1,25 @@
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    minValue = None
+    maxValue = None
+    
+    for e in ints:
+        if minValue is None or minValue > e:
+            minValue = e
+        elif maxValue is None or maxValue < e:
+            maxValue = e
+    return (minValue, maxValue)
+
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
